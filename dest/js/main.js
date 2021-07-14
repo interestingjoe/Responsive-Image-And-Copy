f75fa7412e95892ccaaf62837315d8e15c88e064
Your Name <you@example.com>
2021-07-14 16:36:40 -0700

    Writing comments.

--- dest/js/main.js
+++ dest/js/main.js
@@ -10,25 +10,41 @@
         imageBlock: document.getElementsByClassName('image-block'),
         image: document.getElementById('image'),
         init: () => {
+            $(window).off();
+            main.setAll();
+
             $(window).on('load resize', () => {
-                main.setImageHeight();
-                main.setTextBlockWidth();
-                main.setTextBlockHeight();
+                main.setAll();
             });
+        },
+        setAll: () => {
+            main.setImageHeight();
+            main.setTextBlockWidth();
+            main.setTextBlockHeight();
         },
         setImageHeight: () => {
             let self = this;
-            let naturalHeight = self.image.naturalHeight;
             let windowHeight = window.innerHeight;
 
-            self.image.style.height = (windowHeight <= naturalHeight) ? 
+            self.image.style.height = (windowHeight <= self.image.naturalHeight) ? 
                 windowHeight + 'px' : 
-                naturalHeight + 'px';
+                'auto';
         },
         setTextBlockWidth: () => {
             let self = this;
-            let imagePercent = (self.image.offsetWidth / window.innerWidth) * 100;
-            let half = (100 - imagePercent) / 2;
+            let imageWidthPercent = (self.image.offsetWidth / window.innerWidth) * 100;
+
+            // Cutting the INT down to four digest after the decimal.
+            imageWidthPercent = parseFloat(imageWidthPercent.toFixed(4));
+
+            // I'm using 99.9450 instead of 100 because there is a bug where it doesn't
+            // always inline-block the right panel of copy if I use 100. This works for
+            // now. I would need a lot more time to figure out the bug.
+            let half = (99.9450 - imageWidthPercent) / 2;
+
+            // Cutting the INT down to four digest after the decimal.
+            half = parseFloat(half.toFixed(4));
+
             for (let i = 0; i < main.textBlock.length; i++) {
                 main.textBlock[i].style.width = half + '%';
             }
